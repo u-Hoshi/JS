6d117c86f28fc9b55f5ba7bcb60d3341577b6f9f
u-Hoshi <yuta000707@yahoo.co.jp>
2021-08-12 08:59:18 +0900

    一旦終了

--- qiita-app/src/App.tsx
+++ qiita-app/src/App.tsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import Card from './Card';
 
 function App() {
-  const [data, setData] = useState();
+  const [datas, setData] = useState<any>([]);
   const onClickData = () => {
     const fetchPosts = async () => {
       await axios
-        .get('https://qiita.com/api/v2/authenticated_user/items', {
-          headers: {
-            Authorization: 'Bearer c0825c3521d594040bc59d5404a92374295faa5f',
-          },
-        })
+        .get(
+          'https://qiita.com/api/v2/authenticated_user/items?page=1&per_page=5',
+          {
+            headers: {
+              Authorization: 'Bearer c0825c3521d594040bc59d5404a92374295faa5f',
+            },
+          }
+        )
         .then(res => {
           setData(res.data);
         });
@@ -20,12 +23,63 @@ function App() {
     };
     fetchPosts();
   };
-  console.log(data);
+
+  // XMLHttpRequests
+  const onClickData2 = () => {
+    var oReq = new XMLHttpRequest();
+    oReq.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        if (this.response) {
+          setData(this.response);
+        } else {
+          // エラーを表示する
+          alert(this.status + this.statusText);
+        }
+      }
+    };
+    oReq.open(
+      'GET',
+      'https://qiita.com/api/v2/authenticated_user/items?page=2&per_page=5'
+    );
+    oReq.setRequestHeader(
+      'Authorization',
+      'Bearer c0825c3521d594040bc59d5404a92374295faa5f'
+    );
+    oReq.send();
+    oReq.responseType = 'json';
+  };
+
+  // FetchAPI
+  const onClickData3 = () => {
+    fetch(
+      'https://qiita.com/api/v2/authenticated_user/items?page=3&per_page=5',
+      {
+        headers: {
+          Authorization: 'Bearer c0825c3521d594040bc59d5404a92374295faa5f',
+        },
+      }
+    )
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setData(data);
+      });
+  };
 
   return (
     <>
-      {/* <p>{Object.keys(data).length}</p> */}
-      <button onClick={onClickData}>hoge</button>
+      <button onClick={onClickData}>Axios</button>
+      <button onClick={onClickData2}>XMLHttpRequest</button>
+      <button onClick={onClickData3}>fetchAPI</button>
+      <p>hoge</p>
+      {datas ? (
+        datas?.map((data: any) => {
+          return <Card title={data.title} />;
+        })
+      ) : (
+        <p>false</p>
+      )}
+      <p>bar</p>
     </>
   );
 }
---
+++ qiita-app/src/Card.tsx
@@ -0,0 +1,9 @@
+const Card = (props: any) => {
+  const { title } = props;
+  return (
+    <>
+      <p>{title}</p>
+    </>
+  );
+};
+export default Card;
